70ab817c39bf18b459b578b763f193ca907c0399
Junichi Ito <jit@sonicgarden.jp>
2019-10-13 05:28:08 +0900

    Fix test

--- test/decorator_test.rb
+++ test/decorator_test.rb
@@ -4,11 +4,11 @@ class DecoratorTest < Minitest::Test
   def test_to_decorate
     decorator = Pokeberu::Decorator.new
     input = 'ｱｲｼﾃﾙ'
-    expected = <<TEXT.chomp
-------------------
-|ｱｲｼﾃﾙ           |
-------------------
-TEXT
+    expected = <<-TEXT.chomp
+--------------------
+| ｱｲｼﾃﾙ            |
+--------------------
+    TEXT
 
     assert_equal expected, decorator.decorate(input)
   end
